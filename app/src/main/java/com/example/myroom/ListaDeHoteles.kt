576985e3d1f373297385f16bdf7f73a5c65bf5eb
Jonathan Tejada <jonathan.tejada@epn.edu.ec>
2021-09-15 20:02:54 -0500

    Lista Hoteles Baner superior Interfaz

---
+++ app/src/main/java/com/example/myroom/ListaDeHoteles.kt
@@ -0,0 +1,11 @@
+package com.example.myroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaDeHoteles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_de_hoteles)
+    }
+}
