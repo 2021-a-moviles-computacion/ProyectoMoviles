df71f86fa3eed3473a5d2ead2f11b3035762f4b2
Wilman-Armijos <wilman.armijos@epn.edu.ec>
2021-09-17 11:09:13 -0500

    Recycler View Servicios del Recycler View Habitaciones
    Se hizo el recyycler view de los servicios pero aun falta hacer el de las habitaciones, y el de las otras caracteristicas

--- app/src/main/java/com/example/myroom/MainActivity.kt
+++ app/src/main/java/com/example/myroom/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.myroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
